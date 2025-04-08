ec0ee125e8c7bfcb168541e547c9e70026c52568
josehb96 <josehurtadobaeza@gmail.com>
2025-04-08 02:23:26 +0200

    feat: add Counter component for interactive counting functionality

---
+++ src/components/Counter.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+
+function increment() {
+  count.value++;
+}
+
+function decrement() {
+  count.value--;
+}
+
+</script>
+
+<template>
+  <div>
+    <p>{{ count }}</p>
+    <button class="border px-4 py-2 text-xl" @click="increment">+</button>
+    <button class="border px-4 py-2 text-xl" @click="decrement">-</button>
+  </div>
+</template>
